2a6bab622634e9c92a0e12f3957731a4645c7172
Sasha <sasha7692@gmail.com>
2020-03-19 15:57:53 +0200

    Create task5_2.sh

---
+++ Task2/task5_2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Username of user
+username=$1
+
+# Email of user
+email=$2
+
+# IP address
+ip=$3
+
+# Disable ssh for specific ip
+echo "sshd : $3" | sudo tee -a /etc/hosts.deny
+
+# Text of email
+echo "Hello, Dear $username
+
+I have to notify you that the $(hostname) system ssh connection has been temporarily disabled for you.
+
+Contact the property for more information.
+
+
+Good luck!
+" > mail.txt
+
+# Send email
+echo "Subject: Temporarily ssh disable" | cat - mail.txt | sendmail -t $email
+
+# Remove text of email
+rm mail.txt
