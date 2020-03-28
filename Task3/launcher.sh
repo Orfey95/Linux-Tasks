bf9304a621713f5ec74233220883b79d3d328ace
Sasha <sasha7692@gmail.com>
2020-03-28 11:33:28 +0200

    Update launcher.sh

--- Task3/launcher.sh
+++ Task3/launcher.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+
+
+# Turn on script logging
+set -x
+
+# Check operation system
+if echo $(hostnamectl | grep "Operating System: ") | grep -q "Ubuntu 18.04"; then
+   os="Ubuntu"
+elif echo $(hostnamectl | grep "Operating System: ") | grep -q "CentOS Linux 7"; then
+   os="Centos"
+else
+   echo "Your operating system does not support this script."
+   exit 0
+fi
+
+# Run net_check.sh
+curl https://raw.githubusercontent.com/Orfey95/Linux-Tasks/master/Task3/network_checker.sh | sudo bash 2>&1 | tee mail.txt
+
+
+# Email report 
+# For Ubuntu 18.04
+if [ "$os" = "Ubuntu" ]; then
+   DEBIAN_FRONTEND=noninteractive apt install -y postfix > /dev/null
+   echo "Subject: Logging launcher.sh" | cat - mail.txt | sendmail -t sasha7692@gmail.com
+   rm mail.txt
+fi
+# For Centos 7
+if [ "$os" = "Centos" ]; then
+   echo "Subject: Logging launcher.sh" | cat - mail.txt | sendmail -t sasha7692@gmail.com
+   rm mail.txt
+fi
+
 
